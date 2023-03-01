c6445596dbef68aa39f43051c8f307dd3e5e2f2a
banyo <banyoloper@gmail.com>
2023-03-01 14:33:25 -0600

    Create: URP Materials, Create: Particle System prefa and Trial effect, Implement: Basic Wander Behavior

--- Assets/Scripts/Managers/Player.cs
+++ Assets/Scripts/Managers/Player.cs
@@ -1,9 +1,36 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private ParticleSystem trailParticleSystem;
+
+    [SerializeField] private int bounceParticleEmission = 20;
+    
+    private Vector2 _position;
+
+    private void Awake() => StartNewGame();
+
+    private void StartNewGame()
+    {
+        _position = Vector2.zero;
+        UpdateVisualization();
+        SetTrailEmission(true);
+    }
+
+    private void Update()
+    {
+        UpdateVisualization();
+    }
+
+    public void UpdateVisualization() => trailParticleSystem.transform.localPosition =
+        transform.localPosition;
+    private void SetTrailEmission(bool status)
+    {
+        ParticleSystem.EmissionModule emission = trailParticleSystem.emission;
+        emission.enabled = status;
+    }
 }   
--- Assets/Scripts/Utils/SteeringBehaviors.cs
+++ Assets/Scripts/Utils/SteeringBehaviors.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Collections;
+using JetBrains.Annotations;
+using Random = UnityEngine.Random;
 
 public class SteeringBehaviors : MonoBehaviour
 {
@@ -11,17 +14,25 @@ public class SteeringBehaviors : MonoBehaviour
     [SerializeField, Header("Steering Type")] 
     private SteeringType type;
 
-    [SerializeField] private Transform target;
+    [SerializeField] [CanBeNull] private Transform target;
     [SerializeField] private float speed, maxSpeed, slowingRadius;
     
     private readonly Dictionary<string, Action> _actions = new Dictionary<string, Action>();
     private Vector3 _velocity, _steering, _desiredVelocity;
+
+    [SerializeField, Header("Wander Settings"), Range(1f,10f)]
+    private float timeChange;
+    private Queue<Vector3> _wanderQueue = new Queue<Vector3>();
+    private Vector3 _wanderTarget;
+    private bool _isWandering = false;
+    
 
     private void StartNewGame()
     {
         _actions.Add("Seek", CalculateSeek);
         _actions.Add("RunAway", RunAway);
         _actions.Add("Arrival", Arrival);
+        _actions.Add("Wander", Wander);
     }
 
     private void Awake() => StartNewGame();
@@ -30,6 +41,13 @@ public class SteeringBehaviors : MonoBehaviour
     {
         if (!_actions.TryGetValue(type.ToString(), out var action)) return;
         _desiredVelocity = target.position - transform.position;
+
+        if (type.ToString() != "Wander")
+        {
+            StopAllCoroutines();
+            _isWandering = false;
+        }
+        
         action();
         Move(_steering);
     }
@@ -66,6 +84,34 @@ public class SteeringBehaviors : MonoBehaviour
 
         _desiredVelocity = _desiredVelocity.normalized * factor;
         _steering = _desiredVelocity - _velocity;
+    }
+
+    private void Wander()
+    {
+        CheckQueue();
+        if (!_isWandering) StartCoroutine(wanderSteering());
+        _steering = Seek(_wanderTarget);
+    }
+
+    private void CheckQueue()
+    {
+        if (_wanderQueue.Count > 0) return;
+        for (int i = 0 ;i < 10; i++)
+        {
+            Vector3 randomTarget = new Vector3(Random.Range(-9.2f, 9.2f), 0.2f, Random.Range(-6.6f, 6.6f));
+            _wanderQueue.Enqueue(randomTarget);
+        }
+    }
+    
+    IEnumerator wanderSteering()
+    {
+        _isWandering = true;
+        while (true)
+        {
+            CheckQueue();
+            _wanderTarget = _wanderQueue.Dequeue();
+            yield return new WaitForSeconds(timeChange);
+        }
     }
 
 }
