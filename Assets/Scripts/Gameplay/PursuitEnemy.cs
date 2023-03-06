01e1c6ba7ef86d904f3af2d2ae52a5c4cd3b4249
banyo <banyoloper@gmail.com>
2023-03-05 22:37:14 -0600

    Update: PursuitBehavior and Pursuit Enemy

--- Assets/Scripts/Gameplay/PursuitEnemy.cs
+++ Assets/Scripts/Gameplay/PursuitEnemy.cs
@@ -1,7 +1,21 @@
+using System;
 using UnityEngine;
 
 public class PursuitEnemy : MonoBehaviour
 {
-    
-    
+    public GameObject player;
+    public PursuitBehavior pursuit;
+
+    private Rigidbody playerRb;
+
+    private void Start()
+    {
+        playerRb = player.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        pursuit.target = player.transform;
+        pursuit.targetVelocity = playerRb.velocity;
+    }
 }
--- Assets/Scripts/Utils/Behaviors/PursuitBehavior.cs
+++ Assets/Scripts/Utils/Behaviors/PursuitBehavior.cs
@@ -4,22 +4,20 @@ public class PursuitBehavior : SteeringBehavior
 {
     [SerializeField, Header("Future Frames")]
     private int T = 2;
-
-    [SerializeField] private string targetName = "Player";
-
+    
     private SeekBehavior _seek;
-    private Transform _player;
+    public Transform target;
+    public Vector3 targetVelocity;
     private Vector3 _futurePosition;
 
     void Start()
     {
         _seek = gameObject.GetComponent<SeekBehavior>();
-        _player = GameObject.Find(targetName).transform;
     }
 
     public override Vector3 GetForce()
     {
-        _futurePosition = transform.position + rb.velocity * T;
+        _futurePosition = target.position + targetVelocity * T;
         return _seek.Seek(_futurePosition);
     }
 }
