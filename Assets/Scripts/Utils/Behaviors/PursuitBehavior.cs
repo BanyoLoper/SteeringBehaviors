947dd630fc1511b43a9e601d73bf9941a1d3d002
banyo <banyoloper@gmail.com>
2023-03-05 22:41:42 -0600

    Update: PursuitBehavior, Delete: PursuitEnemy.cs as it was not needed.

--- Assets/Scripts/Utils/Behaviors/PursuitBehavior.cs
+++ Assets/Scripts/Utils/Behaviors/PursuitBehavior.cs
@@ -2,22 +2,24 @@ using UnityEngine;
 
 public class PursuitBehavior : SteeringBehavior
 {
+    public GameObject target;
     [SerializeField, Header("Future Frames")]
     private int T = 2;
     
     private SeekBehavior _seek;
-    public Transform target;
-    public Vector3 targetVelocity;
+
     private Vector3 _futurePosition;
+    private Rigidbody _targetRb;
 
     void Start()
     {
         _seek = gameObject.GetComponent<SeekBehavior>();
+        _targetRb = target.GetComponent<Rigidbody>();
     }
 
     public override Vector3 GetForce()
     {
-        _futurePosition = target.position + targetVelocity * T;
+        _futurePosition = target.transform.position + _targetRb.velocity * T;
         return _seek.Seek(_futurePosition);
     }
 }
