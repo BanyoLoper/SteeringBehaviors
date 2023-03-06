a3b96c05daf8b8a842df21ad6d2ac4f3a486b52f
banyo <banyoloper@gmail.com>
2023-03-05 22:10:59 -0600

    Implement: ArrivalBehavior

--- Assets/Scripts/Utils/Behaviors/ArrivalBehavior.cs
+++ Assets/Scripts/Utils/Behaviors/ArrivalBehavior.cs
@@ -2,19 +2,33 @@ using UnityEngine;
 
 public class ArrivalBehavior : SteeringBehavior
 {
-    [SerializeField] private string targetName = "Player"; 
-        
+    [SerializeField] private float slowingRadius = 3f;
     private SeekBehavior _seek;
-    private Transform _player;
+    public Vector3 seekPoint;
+    
+    
     void Start()
     {
         _seek = gameObject.GetComponent<SeekBehavior>();
-        _player = GameObject.Find(targetName).transform;
     }
 
     public override Vector3 GetForce()
     {
-        return _seek.Seek(_player.position) * -1f;
+        return Arrive(seekPoint);
+    }
+
+    public Vector3 Arrive(Vector3 target)
+    {
+        DesiredVelocity = (target - transform.position);
+        float distance = DesiredVelocity.magnitude;
+        float factor = maxSpeed;
+        if (distance < slowingRadius)
+        {
+            factor = maxSpeed * (distance / slowingRadius);
+        }
+        
+        DesiredVelocity = DesiredVelocity.normalized * (maxSpeed * factor);
+        return DesiredVelocity - rb.velocity;
     }
     
 }
