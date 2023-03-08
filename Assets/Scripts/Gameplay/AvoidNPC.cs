40502d0aa4d1e779a16bf0dd32d9c0b4b138b315
banyo <banyoloper@gmail.com>
2023-03-07 20:03:34 -0600

    Implement: click target in AvoidNPC class, update reset position and seekPoint

--- Assets/Scripts/Gameplay/AvoidNPC.cs
+++ Assets/Scripts/Gameplay/AvoidNPC.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class AvoidNPC : MonoBehaviour
 {
 
     [SerializeField] private Transform goal;
     [SerializeField] private Transform start;
-
+    [SerializeField] private Plane plane;
+    
     private SeekBehavior _seek;
 
     private void Start()
@@ -15,8 +18,14 @@ public class AvoidNPC : MonoBehaviour
         _seek.seekPoint = goal.position;
     }
 
+    private void Update()
+    {
+        _seek.seekPoint = plane.MouseClick == Vector3.zero ? goal.position : plane.MouseClick;
+    }
+
     private void OnMouseDown()
     {
         transform.position = start.position;
+        plane.MouseClick = Vector3.zero;
     }
 }
